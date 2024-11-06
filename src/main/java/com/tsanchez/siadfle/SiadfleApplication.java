c9c109c5b2842f0f46171b4889c83660b67b9688
armandoAsus <armando.sa626@gmail.com>
2024-11-06 10:00:18 -0600

    Commit inicial

---
+++ src/main/java/com/tsanchez/siadfle/SiadfleApplication.java
@@ -0,0 +1,13 @@
+package com.tsanchez.siadfle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiadfleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiadfleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tsanchez/siadfle/SiadfleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsanchez.siadfle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiadfleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
